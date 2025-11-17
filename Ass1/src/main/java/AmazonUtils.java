4e5cae077ef3d6a6bff6e1904da996d950ec886a
Eden Leyba <edenleyba44@gmail.com>
2025-11-18 00:16:59 +0200

    LocalApp implementation basics using AmazonUtils

---
+++ Ass1/src/main/java/AmazonUtils.java
@@ -0,0 +1,167 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.CompleteMultipartUploadRequest;
+import software.amazon.awssdk.services.s3.model.CompletedMultipartUpload;
+import software.amazon.awssdk.services.s3.model.CompletedPart;
+import software.amazon.awssdk.services.s3.model.CreateBucketConfiguration;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.CreateMultipartUploadRequest;
+import software.amazon.awssdk.services.s3.model.CreateMultipartUploadResponse;
+import software.amazon.awssdk.services.s3.model.DeleteBucketRequest;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Object;
+import software.amazon.awssdk.services.s3.model.UploadPartRequest;
+import software.amazon.awssdk.services.s3.paginators.ListObjectsV2Iterable;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.core.sync.ResponseTransformer;
+import software.amazon.awssdk.services.s3.model.*;
+import software.amazon.awssdk.core.exception.*;
+
+public class AmazonUtils {
+    private static S3Client s3;
+    private static final int mb = 1024 * 1024;
+    private static final int LARGE_FILE_SIZE = 100 * mb;
+    private static Region region;
+
+    public AmazonUtils(Region r) {
+        region = r;
+        s3 = S3Client.builder().region(region).build();
+    }
+
+    public static void createBucket(String bucket) throws S3Exception, SdkClientException {
+        if (region == Region.US_EAST_1) {
+            s3.createBucket(CreateBucketRequest.builder()
+                    .bucket(bucket)
+                    .build());
+        } else {
+            s3.createBucket(CreateBucketRequest.builder()
+                    .bucket(bucket)
+                    .createBucketConfiguration(
+                            CreateBucketConfiguration.builder()
+                                    .locationConstraint(region.id())
+                                    .build())
+                    .build());
+        }
+
+        System.out.println(bucket);
+    }
+
+    public static void S3UploadFile(String bucketName, String key, File file) throws IOException, S3Exception, SdkClientException {
+        if(file.length() > LARGE_FILE_SIZE) {
+            multipartUpload(bucketName, key, file);
+        }
+        else {
+            PutObject(bucketName, key, file);
+        }
+    }
+
+    public static void S3GetSmallFile(String bucketName, String key) throws IOException, SdkClientException {
+        try {
+            GetObjectRequest request = GetObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build();
+
+            ResponseBytes<?> objectBytes =
+                    s3.getObject(request, ResponseTransformer.toBytes());
+
+            byte[] data = objectBytes.asByteArray();
+            String text = new String(data);
+
+            System.out.println("Object content:");
+            System.out.println(text);
+
+        } catch (S3Exception e) {
+            System.err.println("S3 error: " + e.awsErrorDetails().errorMessage());
+        }
+    }
+
+    private static void PutObject(String bucketName, String key, File file) throws IOException, S3Exception, SdkClientException {
+        PutObjectRequest putRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .contentType("text/plain")
+                .build();
+
+        s3.putObject(putRequest, RequestBody.fromInputStream(new FileInputStream(file), file.length()));
+    }
+
+    private static void multipartUpload(String bucketName, String key, File file) throws IOException, S3Exception, SdkClientException {
+        //minimum number of bytes in one part according to aws docs: https://docs.aws.amazon.com/AmazonS3/latest/userguide/qfacts.html
+        int part_num_bytes = 5 * mb;
+
+        // First create a multipart upload and get upload id
+        CreateMultipartUploadRequest createMultipartUploadRequest = CreateMultipartUploadRequest.builder()
+                .bucket(bucketName).key(key)
+                .build();
+        CreateMultipartUploadResponse response = s3.createMultipartUpload(createMultipartUploadRequest);
+        String uploadId = response.uploadId();
+        System.out.println(uploadId);
+
+        // Split the file into parts with part_num_bytes in each of them, and Upload all the different parts of the object
+        FileInputStream fis = new FileInputStream(file);
+
+        byte[] buffer = new byte[part_num_bytes];
+        int partNumber = 1;
+        int bytesRead;
+        List<CompletedPart> completedParts = new ArrayList<>();
+
+        while ((bytesRead = fis.read(buffer)) != -1) {
+            ByteBuffer byteBuffer = ByteBuffer.wrap(buffer.clone(), 0, bytesRead);
+
+            UploadPartRequest uploadPartRequest = UploadPartRequest
+                    .builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .uploadId(uploadId)
+                    .partNumber(partNumber).build();
+
+            String etag = s3.uploadPart(
+                    uploadPartRequest,
+                    RequestBody.fromByteBuffer(byteBuffer)
+            ).eTag();
+
+            CompletedPart part = CompletedPart
+                    .builder()
+                    .partNumber(partNumber)
+                    .eTag(etag)
+                    .build();
+            completedParts.add(part);
+
+            partNumber++;
+        }
+
+        fis.close();
+
+        CompletedMultipartUpload completedMultipartUpload = CompletedMultipartUpload
+                .builder()
+                .parts(completedParts)
+                .build();
+
+        CompleteMultipartUploadRequest completeMultipartUploadRequest = CompleteMultipartUploadRequest
+                .builder()
+                .bucket(bucketName)
+                .key(key)
+                .uploadId(uploadId)
+                .multipartUpload(completedMultipartUpload).build();
+
+        s3.completeMultipartUpload(completeMultipartUploadRequest);
+    }
+
+}
--- Ass1/src/main/java/LocalApp.java
+++ Ass1/src/main/java/LocalApp.java
@@ -1,5 +1,229 @@
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ec2.Ec2Client;
+import software.amazon.awssdk.services.ec2.model.*;
+import software.amazon.awssdk.services.ec2.model.Tag;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
+import software.amazon.awssdk.core.exception.*;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Base64;
+import java.util.List;
+
 public class LocalApp {
+    static Ec2Client ec2;
+    static String manager_role_value = "manager";
+    static String bucket_name;
+    static Region region = Region.US_EAST_1;
+
+    public static void RunManagerEC2Instance() throws Ec2Exception {
+        String amiId = "ami-0cae6d6fe6048ca2c";
+        String role = manager_role_value;
+
+        String script = "echo 'This machine is running'"; //run the manager jar
+        RunInstancesRequest runRequest = RunInstancesRequest.builder()
+                .instanceType(InstanceType.T1_MICRO)
+                .imageId(amiId)
+                .maxCount(1)
+                .minCount(1)
+                .keyName("dsp_lab")
+                .userData(Base64.getEncoder().encodeToString(script.getBytes()))
+                .build();
+
+        RunInstancesResponse response = ec2.runInstances(runRequest);
+
+        String instanceId = response.instances().get(0).instanceId();
+
+        Tag tag = Tag.builder() //Role:manager
+                .key("Role")
+                .value(role)
+                .build();
+
+        CreateTagsRequest tagRequest = CreateTagsRequest.builder()
+                .resources(instanceId)
+                .tags(tag)
+                .build();
+
+        ec2.createTags(tagRequest);
+        System.out.printf(
+                "Successfully started EC2 Manager instance %s based on AMI %s\n",
+                instanceId, amiId);
+    }
+
+    public static boolean is_manager_running() {
+        DescribeInstancesRequest req = DescribeInstancesRequest.builder()
+                .filters(
+                        Filter.builder().name("tag:Role").values(manager_role_value).build(),
+                        Filter.builder().name("instance-state-name").values("pending", "running").build()
+                )
+                .build();
+        DescribeInstancesResponse response = ec2.describeInstances(req);
+
+        for (Reservation reservation : response.reservations()) {
+            for (Instance instance : reservation.instances()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ec2 = Ec2Client.builder()
+                .region(region)
+                .build();
+
+        String  inputFileName   = args[0],
+                outputFileName  = args[1];
+        int     n               = Integer.parseInt(args[2]);
+
+        if (is_manager_running()) {
+            System.out.println("Manager is running!");
+        } else {
+            try {
+                RunManagerEC2Instance();
+            } catch (Ec2Exception e) {
+                System.out.println("Could not run EC2 Manager instance: " + e.getMessage());
+                ec2.close();
+                System.exit(1);
+            }
+        }
+
+        //Initialize AmazonUtils Object
+        AmazonUtils amazonUtils = new AmazonUtils(region);
+
+        bucket_name = "dsp1-task1-" + System.currentTimeMillis();
+        //Create a bucket
+        try {
+            AmazonUtils.createBucket(bucket_name);
+        }
+        catch (S3Exception e) {
+            System.err.println("AWS S3 error: " + e.awsErrorDetails().errorMessage());
+        }
+        catch (SdkClientException e) {
+            System.err.println("Client-side error: " + e.getMessage());
+        }
+
+        //Upload the input file to S3
+        File inputFile = new File(inputFileName);
+        String key = "local-app-" + inputFileName;
+        try {
+            AmazonUtils.S3UploadFile(bucket_name, key, inputFile);
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+        }
+        catch (S3Exception e) {
+            System.err.println("AWS S3 error: " + e.awsErrorDetails().errorMessage());
+        }
+        catch (SdkClientException e) {
+            System.err.println("Client-side error: " + e.getMessage());
+        }
+
+        // SQS
+        SqsClient sqs = SqsClient.builder().region(region).build();
+
+        // Create LocalsOutput SQS queue
+        String queue_name = "LocalsOutput";
+        try {
+            CreateQueueRequest request = CreateQueueRequest.builder()
+                    .queueName(queue_name)
+                    .build();
+            CreateQueueResponse create_result = sqs.createQueue(request);
+        } catch (QueueNameExistsException e) {
+            System.err.println("QueueNameExistsException: " + e.getMessage());
+        }
+
+        GetQueueUrlRequest getQueueRequest = GetQueueUrlRequest.builder()
+                .queueName(queue_name)
+                .build();
+        String queueUrl = sqs.getQueueUrl(getQueueRequest).queueUrl();
+        System.out.println(queueUrl);
+
+        // send message
+        String message_body = bucket_name + "\n" + key;
+        SendMessageRequest send_msg_request = SendMessageRequest.builder()
+                .queueUrl(queueUrl)
+                .messageBody(message_body)
+                .build();
+        sqs.sendMessage(send_msg_request);
+        System.out.println("Sent message: " + message_body);
+
+        //receive message
+        ReceiveMessageRequest receiveRequest = ReceiveMessageRequest.builder()
+                .queueUrl(queueUrl)
+                .build();
+        List<Message> messages = sqs.receiveMessage(receiveRequest).messages();
+
+        String received_message = messages.getFirst().body();
+        System.out.println("Received message: " + received_message);
+        String[] received_message_parts = received_message.split("\n");
+        String received_bucket_name = received_message_parts[0];
+        String received_key = received_message_parts[1];
+
+        //Get the file from S3
+        try {
+            AmazonUtils.S3GetSmallFile(received_bucket_name, received_key);
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+        }
+        catch (S3Exception e) {
+            System.err.println("AWS S3 error: " + e.awsErrorDetails().errorMessage());
+        }
+        catch (SdkClientException e) {
+            System.err.println("Client-side error: " + e.getMessage());
+        }
+
+
+
+        ec2.close();
+    }
+
+    public static void DeleteAllS3Buckets() {
+        //Delete all s3 buckets
+        S3Client s3 = S3Client.builder().region(region).build();
+
+        ListBucketsResponse bucketsResponse = s3.listBuckets();
+
+        for (Bucket bucket : bucketsResponse.buckets()) {
+            String bucketName = bucket.name();
+
+            try {
+                // List all objects in the bucket
+                ListObjectsV2Request listReq = ListObjectsV2Request.builder()
+                        .bucket(bucketName)
+                        .build();
+
+                ListObjectsV2Response listRes = s3.listObjectsV2(listReq);
+
+                // Loop and delete each object
+                for (S3Object obj : listRes.contents()) {
+                    System.out.println("Deleting: " + obj.key());
+                    DeleteObjectRequest deleteReq = DeleteObjectRequest.builder()
+                            .bucket(bucketName)
+                            .key(obj.key())
+                            .build();
+
+                    s3.deleteObject(deleteReq);
+                }
+
+                System.out.println("All objects deleted.");
+            } catch (S3Exception e) {
+                System.err.println(e.awsErrorDetails().errorMessage());
+            }
+
+            DeleteBucketRequest deleteBucketRequest = DeleteBucketRequest.builder().bucket(bucket.name()).build();
+            s3.deleteBucket(deleteBucketRequest);
+            System.out.println("Deleted bucket: " + bucket.name());
+        }
+
+        s3.close();
+
     }
 }
